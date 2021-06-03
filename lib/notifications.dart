2c571c091ed740001f6102443d97179501dd1905
svssp <prakashsvss8@gmail.com>
2021-06-03 16:32:18 +0530

    placement statistics

--- lib/notifications.dart
+++ lib/notifications.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-List<List<String>>s=[["Infosys","Eligibility Criteria like grade points or Attendance to apply for the company Comes here","31-12-2019"],
-["Infosys","Eligibility Criteria like grade points or Attendance to apply for the company Comes here","31-12-2019"],
-["Infosys","Eligibility Criteria like grade points or Attendance to apply for the company Comes here","31-12-2019"],
-["Infosys","Eligibility Criteria like grade points or Attendance to apply for the company Comes here","31-12-2019"]];
+class Companies
+{
+  String name;
+  List<String>eligibility;
+  String date;
+
+  Companies(this.name,this.eligibility,this.date);
+}
+
+List<Companies>s=[Companies("Infosys",["Candidate Should be Human","Canditate should eat both veg and non-veg"],"30-12-2021"),
+Companies("Wipro",["Candidate Should be Alien","Canditate must donate an UFO to company"],"30-12-2022"),
+Companies("Cognizant",["Candidate Should be from China","Canditate should eat Aliens also"],"31-12-2023"),
+Companies("Yahoo",["Candidate Should be yoho","Canditate must wear goggles"],"10-22-2021"),
+];
 
 class Notifications extends StatefulWidget {
   @override
@@ -20,7 +31,7 @@ class _NotificationsState extends State<Notifications> {
     fontFamily: 'inter',
     textTheme: TextTheme(
       headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-      headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+      
       )),
 
 
@@ -34,48 +45,94 @@ class _NotificationsState extends State<Notifications> {
             icon: Icon(Icons.arrow_back),
             onPressed: ()=>Navigator.of(context).pop(),
           ),
-          title: new Text("Notifications"),
+          title: new Text("Notifications",style:Theme.of(context).textTheme.headline5.copyWith(fontWeight:FontWeight.bold),
+          ),
           centerTitle: true,
           elevation: 0,
         ),
 
         body: SingleChildScrollView(
+          
           child: Container(
+            color:Colors.grey[100],
             child:Column(
+
               children: s.map(
                 (company) => Container(
                   decoration: BoxDecoration(
-                        border:Border(bottom: BorderSide(width:1.0,color:Colors.grey),/*top:BorderSide(width:1.0,color:Colors.grey)*/)
+                        border:Border(bottom: BorderSide(width:1.0,color:Colors.grey[400]),/*top:BorderSide(width:1.0,color:Colors.grey)*/)
                       ),
+
                   child: Padding(
-                    padding: const EdgeInsets.fromLTRB(12.0, 5.0, 0.0, 6.0),
+
+                    padding: const EdgeInsets.fromLTRB(12.0, 14.0, 4.0, 6.0),
                     child: Container(
                       width: double.infinity,
                       
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(company[0],
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20.0,
-                          )
+
+
+                          Align(
+                            alignment: Alignment.center,
+                            child: Text(company.name.toUpperCase(),
+                            style: GoogleFonts.openSans(textStyle:TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 20.0,
+                            ),)
+                            ),
                           ),
+
                           SizedBox(height:8),
-                          Text("Eligibility Criteria :",style: TextStyle(
-                            fontWeight:FontWeight.bold,
+
+                          Text("Eligibility Criteria :",style:GoogleFonts.montserrat(textStyle:TextStyle(
+                            fontWeight:FontWeight.w500,
                             fontSize: 17.0,
                             ),
                             ),
-                          SizedBox(height:4),
-                          Text(company[1],style: TextStyle(
-                            fontSize: 16.0,
-                            ),),
-                          SizedBox(height:8),
-                          Text("Last Date For Application : "+company[2],
-                          style: TextStyle(fontSize: 15.0),
                           ),
-                          Divider(height:20,color: Colors.grey[350]),
+
+                          SizedBox(height:4),
+
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: company.eligibility.map(
+                              (eligibility) => Padding(
+                                padding: const EdgeInsets.fromLTRB(8.0,2.0,0.0,2.0),
+                                child: Container(
+                                  child:Text((company.eligibility.indexOf(eligibility)+1).toString()+". "+eligibility,
+                                  style: GoogleFonts.play(
+                                     textStyle: TextStyle(
+                              fontSize:16.0,
+                              fontWeight: FontWeight.w300,
+                            )
+                          ),
+                                )),
+                              ),
+                              ).toList(),
+                          ),
+
+                          SizedBox(height:8),
+
+                          Text.rich(TextSpan(children: [
+                            TextSpan(
+                              text:"Last Date To Apply: ",
+                              style: GoogleFonts.breeSerif(
+                                textStyle: TextStyle(
+                                  fontSize:15.0
+                                )
+                              ),
+                            )
+                            ,
+                            TextSpan(
+                              text:company.date,
+                              style:TextStyle(color: Colors.blue[400],fontSize:15.0,fontWeight: FontWeight.bold),
+                            )
+                          ])),
+
+                          Divider(height:30,color: Colors.grey[300]),
+
                           Align(
                             alignment: Alignment.centerRight,
                               child: TextButton(
@@ -84,14 +141,18 @@ class _NotificationsState extends State<Notifications> {
                                 minimumSize: Size(20, 10),
                                 tapTargetSize: MaterialTapTargetSize.shrinkWrap,
                                 ),
-                              child: Text("Apply now >>>",textAlign: TextAlign.right,),
+                              child: Text("Apply now >>",textAlign: TextAlign.right,),
                               autofocus: false,
                               onPressed: (){},
                               ),
                           ),
+                          SizedBox(
+                            height:10,
+                          ),
                         ],),
                     ),
                   ),
+                
                 )).toList(),
             ),
           ),
--- lib/studentsplaced.dart
+++ lib/studentsplaced.dart
@@ -1,37 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:fl_chart/fl_chart.dart';
 
+
+List <PieChartSectionData> _placements=[
+  PieChartSectionData(
+    color:Colors.cyan,
+    value: 4000/131,
+    radius: 30,
+    showTitle: false,
+    title: "3.6LPA",
+  ),
+  PieChartSectionData(
+    color: Colors.lightBlue,
+    value: 3000/131,
+    radius: 30,
+    showTitle: false,
+    title: "4.2lpa",
+  ),
+  PieChartSectionData(
+    color: Colors.green,
+    value: 3600/131,
+    radius: 30,
+    showTitle: false,
+    title: "4.5lpa",
+  ),
+  PieChartSectionData(
+    color: Colors.orange,
+    value: 1500/131,
+    radius: 30,
+    showTitle: false,
+    title: "6lpa",
+  ),
+  PieChartSectionData(
+    color: Colors.lightBlue,
+    value: 800/131,
+    radius: 30,
+    showTitle: false,
+    title: "8lpa",
+  ),
+  PieChartSectionData(
+    color: Colors.deepPurple,
+    value: 200/131,
+    radius: 30,
+    showTitle: false,
+    title: "12lpa",
+  ),
+];
+
 class StudentsPlaced extends StatefulWidget {
   @override
   _StudentsPlacedState createState() => _StudentsPlacedState();
 }
 
 class _StudentsPlacedState extends State<StudentsPlaced> {
-
-  List<PieChartSectionData> _sections= List<PieChartSectionData>();
-
-  @override
-  void initState(){
-    super.initState();
-
-    PieChartSectionData _item1=PieChartSectionData(
-      value: 34,
-      title: "3.6 LPA",
-      titleStyle: TextStyle(color: Colors.black38),
-    );
-    PieChartSectionData _item2=PieChartSectionData(
-      value: 40,
-      title: "4.8 LPA",
-      titleStyle: TextStyle(color: Colors.black38),
-    );
-    PieChartSectionData _item3=PieChartSectionData(
-      value: 26,
-      title: "6.0 LPA",
-      titleStyle: TextStyle(color: Colors.black38),
-    );
-
-    _sections=[_item1,_item2,_item3];
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -55,19 +76,117 @@ class _StudentsPlacedState extends State<StudentsPlaced> {
             onPressed: ()=>Navigator.of(context).pop(),
           ),
           centerTitle: true,
+          elevation: 0,
           title: Text("Students Placed"),
           
         ),
 
 
-        body: PieChart(
-          PieChartData(
-            sections: _sections,
-            borderData: FlBorderData(show: false),
-            sectionsSpace: 0,
+        body: SingleChildScrollView(
+                  child: Column(
+            children: [
+              AspectRatio(
+                aspectRatio: 1.4,
+                              child: PieChart(
+                    PieChartData(
+                      sections: _placements,
+                      borderData: FlBorderData(show:false),
+                      centerSpaceRadius: 40,
+                      sectionsSpace: 0,
+                      startDegreeOffset: 90,
+                      
+                    ),  
+                  ),
+              ),
+
+              Wrap(
+                alignment: WrapAlignment.spaceEvenly,
+                direction: Axis.horizontal,                 
+                  children: _placements.map(
+                    (package) => Padding(
+                      padding: const EdgeInsets.fromLTRB(4.0,0.0,8.0,10.0),
+                      child: Container(
+                        width: 80,
+                        child: Row(
+                          children:[
+                            Container(
+                              decoration:BoxDecoration(
+                                shape:BoxShape.circle,
+                                color: package.color,
+                              ),
+                              height: 18,
+                              width: 18,
+                              
+                            ),
+                            SizedBox(width:4),
+                            Text(package.title,
+                            style:TextStyle(color: Colors.black,fontWeight: FontWeight.w600,fontSize: 16),
+                            ),
+                            ]
+                        ),
+                      ),
+                    )).toList(),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(18.0,4.0,0,0),
+                      child: Container(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          
+                          SizedBox(height:12),
+                          Text("Total number of placements: "+"172",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                          SizedBox(height:12),
+                          Text("Total Number Of Students Participated: "+"120",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                          SizedBox(height:12),
+                          Text("Percentage of Students Placed: "+"64%",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                          SizedBox(height:12),
+                          Text("Heighest Package Offered: "+"12 LPA",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                          SizedBox(height:12),
+                          Text("Highest Package offered Company: "+"Name Marchipoyya",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                          SizedBox(height:12),
+                          Text("Student who got Highest Package : "+"JP, VB",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                          SizedBox(height:12),
+                          Text("Average Package: "+"3.6 LPA",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                          SizedBox(height:12),
+                          Text("Median Package: "+"3.6 LPA",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                          SizedBox(height:12),
+                          Text("Company Offering Higher Placements: "+"Telidhu",
+                          style: TextStyle(
+                            fontSize:18,
+                          ),),
+                        ],
+                      )),
+                    ),
+                ),
+            ],
           ),
-        
-          
         ),
     )
     );
